ee2809496b419180b378ba4e8789e1ed5f6da5aa
Nachwahl <robin@ferch-mail.de>
2019-06-08 00:07:24 +0200

    Added Walking Animation

---
+++ AnimatedSprite.gd
@@ -0,0 +1,25 @@
+extends AnimatedSprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+    var velocity = Vector2()  # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * 50
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.stop()
