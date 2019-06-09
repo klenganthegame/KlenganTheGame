78d162eee487433850346d6e7ca9c8d570565c6b
Max Schmitt <max98tonton@gmail.com>
2019-06-09 15:48:59 +0200

    added signal also to klengan

--- objects/level1/Klengan.gd
+++ objects/level1/Klengan.gd
@@ -14,6 +14,8 @@ var can_interact = false
 var can_move = true
 var area : Area2D
 
+signal dialogue_exit()
+
 func _ready():
 	pass    
 
@@ -76,15 +78,15 @@ func talk(text : Array):
 func _on_Area2D_area_entered(_area):
 	can_interact = true
 	area = _area
-	pass # Replace with function body.
+	pass
 
 
 func _on_Area2D_area_exited(area):
 	can_interact = false
 	area = null
-	pass # Replace with function body.
+	pass
 
 
 func _on_DialogueBox_dialogue_exit():
-	# do stuff here
+	emit_signal("dialogue_exit")
 	pass
