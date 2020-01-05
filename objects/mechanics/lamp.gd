f04dc3fbf37c2f29c5c581fe2d37bc0598038d92
Max Schmitt <max98tonton@gmail.com>
2020-01-05 18:40:33 +0100

    moved sound to audiohandler to fix loudness-bug

--- objects/mechanics/lamp.gd
+++ objects/mechanics/lamp.gd
@@ -1,19 +1,11 @@
 extends Node2D
 
-export var message_sound : AudioStreamSample = load("res://resources/audio/button.wav")
-var audio : AudioStreamPlayer
+
 
 export var is_on : bool = false
 
 func _ready():
-	message_sound.loop_mode = message_sound.LOOP_DISABLED
-	message_sound.loop_begin = 0
-	message_sound.loop_end = 0
-	
-	audio = AudioStreamPlayer.new()
-	audio.stream = message_sound
-	add_child(audio)
-	
+
 	if is_on:
 		on()
 	else:
@@ -28,7 +20,7 @@ func off():
 	$on.hide()
 	
 func trigger():
-	audio.play(0)
+	AudioHandler.play_button_sound()
 	is_on = !is_on
 	if is_on:
 		on()
