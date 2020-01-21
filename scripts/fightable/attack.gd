c73929177dec23eb483d477d2a98fd964432c0d0
Max Schmitt <max98tonton@gmail.com>
2020-01-21 23:44:43 +0100

    #50 added initializer

--- scripts/fightable/attack.gd
+++ scripts/fightable/attack.gd
@@ -2,5 +2,11 @@ class_name Attack
 
 var id : int 
 var atk : int
-var reload_time : int = 0 
-var distance : int = 0
+var reload_time : float = 0 
+var distance : float = 0
+
+func _init(var m_id : int, m_atk : int, m_reload_time : int, m_distance : float):
+	id = id
+	atk = m_atk
+	reload_time = m_reload_time
+	distance = m_distance
