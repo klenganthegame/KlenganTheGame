2f94aea2830ecc04fd5eb0d4d7edcff02f7ef4f5
Max Schmitt <max98tonton@gmail.com>
2020-01-23 00:15:45 +0100

    menu: implemented youtube and reddit button

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -21,3 +21,12 @@ func _on_Credits_pressed():
 func _on_Exit_pressed():
 	#get_tree().change_scene(SCENES.Meni)
 	pass # Replace with function body.
+
+
+func _on_Reddit_pressed():
+	OS.shell_open("https://www.reddit.com/r/Klengan/")
+	pass # Replace with function body.
+
+func _on_Youtube_pressed():
+	OS.shell_open("https://www.youtube.com/channel/UC9N0MRIXnKo03d0mmZ3BwPA")
+	pass # Replace with function body.
