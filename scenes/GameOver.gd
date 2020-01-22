e5cb70cd568577fa73a85c3869d3ffc0bf8e91e2
Max Schmitt <max98tonton@gmail.com>
2020-01-22 10:30:07 +0100

    #26 implemented GameOverScreen

--- scenes/GameOver.gd
+++ scenes/GameOver.gd
@@ -4,3 +4,11 @@ func _ready():
 	$AnimationPlayer.play("idle")
 	$Klengan.play("walk_in")
 	pass
+
+
+func _on_Load_pressed():
+	pass # Replace with function body.
+
+
+func _on_Menu_pressed():
+	pass # Replace with function body.
