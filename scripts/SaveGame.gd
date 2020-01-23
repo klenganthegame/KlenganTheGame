0b3b57cf08e2e3476d0196b9a67fc3c504526d33
Max Schmitt <max98tonton@gmail.com>
2020-01-23 11:17:09 +0100

    added save_game script

--- scripts/SaveGame.gd
+++ scripts/SaveGame.gd
@@ -1,4 +1,33 @@
 extends Node
 
-func _ready():
-	pass
+const LIFE = "life"
+const SCORE = "score"
+const LEVELSTATE = "levelstate"
+const save_const = "data"
+
+const path = "usr://save.data"
+
+func save_level(life: int, score : int, levelstate : int) -> bool:
+	var savedict = to_dict(life, score, levelstate)
+	var file = File.new()
+	file.open(path, file.WRITE)
+	file.store_line(JSON.print(savedict))
+	file.close()
+	return true
+
+func load_level() -> Dictionary:
+	var file = File.new()
+	var error = file.open(path, file.READ)
+	var text = file.get_as_text()
+	var r : JSONParseResult = JSON.parse(text)
+	return r[save_const]
+
+func to_dict(life: int, score : int, levelstate : int) -> Dictionary:
+	var result = {
+		save_const : {
+			LIFE : life,
+			SCORE : score,
+			LEVELSTATE : levelstate
+		}
+	}
+	return {}
