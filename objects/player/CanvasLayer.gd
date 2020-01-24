740242fdb42f8a78311747caf06beb107b6f9ff4
Max Schmitt <max98tonton@gmail.com>
2020-01-24 12:49:11 +0100

    sexier transitions

--- objects/player/CanvasLayer.gd
+++ objects/player/CanvasLayer.gd
@@ -23,9 +23,20 @@ func _on_Save_pressed():
 
 
 func _on_Settings_pressed():
+	$PauseMenu.play("settings_in")
 	pass # Replace with function body.
 
 
 func _on_BackToMenu_pressed():
+	$PauseMenu.play("backtomenu")
+
+func go_to_Menu():
 	get_tree().change_scene(SCENES.Menu)
-	pass # Replace with function body.
+
+
+func _on_OptionButton_item_selected(ID):
+	get_node("Colorblindness").Type = ID
+	print("selected")
+
+func _on_BackToPause_pressed():
+	$PauseMenu.play("settings_out")
