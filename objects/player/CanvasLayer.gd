e25f0e2b4e778ec0e062343904deb89d0374a056
Sven Iffland <sven@aiffland.de>
2020-01-30 21:41:41 +0100

    Bisschen stuff Organisiert
    Collision Samples
    Esc für pause und Pause verlassen

--- objects/player/CanvasLayer.gd
+++ objects/player/CanvasLayer.gd
@@ -6,10 +6,20 @@ func _ready():
 
 func _on_Pause_pressed():
 	$PauseMenu.play("in")
+	get_parent().paused = true 
 	pass # Replace with function body.
 
+func _unhandled_key_input(event):
+	if Input.is_action_just_pressed("pause"):
+		if get_parent().paused == true :
+			$PauseMenu.play("out")
+		else:
+			$PauseMenu.play("in")
+		get_parent().paused = !get_parent().paused 
+	pass # Replace with function body.
 
 func _on_Back_pressed():
+	get_parent().paused = false 
 	$PauseMenu.play("out")
 	pass # Replace with function body.
 
