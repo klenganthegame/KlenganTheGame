09d933e5ac08b3993dfe95b88272edb41c11c85b
Max Schmitt <max98tonton@gmail.com>
2020-02-02 14:06:15 +0100

    implemented pseudo3D level + added some tutorial level sprites

--- scripts/scene_paths.gd
+++ scripts/scene_paths.gd
@@ -3,4 +3,4 @@ extends Node
 const Menu = "res://scenes/Menu.tscn"
 const GameOver = "res://scenes/GameOver.tscn"
 
-const Level1 = "res://level/1/Level1.tscn"
+const Level1 = "res://level/1/Level1inpseudo3D.tscn"
