1fdba0775ceb63093568e2cc18da4ae5f13e7954
eisclimber <dreililu@gmx.de>
2020-02-07 22:31:12 +0100

    FIxed Sneak bug
    -Sneak now works when pressing sneak in idle and then walking

--- objects/mechanics/finite_state_machine/states_klengan/movement/idle.gd
+++ objects/mechanics/finite_state_machine/states_klengan/movement/idle.gd
@@ -8,7 +8,7 @@ func enter():
 
 func update(delta):
 	.update(delta)
-	if get_input_direction() != Vector2():
-		emit_signal("finished", "move")
-	elif get_input_direction() != Vector2() and Input.is_action_pressed("sneak"):
+	if get_input_direction() != Vector2() and Input.is_action_pressed("sneak"):
 		emit_signal("finished","sneak")
+	elif get_input_direction() != Vector2():
+		emit_signal("finished", "move")
