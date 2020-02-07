66b9fcd004b4c5f8572aaf45b669efd72ec63ff4
Sven Iffland <sven@aiffland.de>
2020-02-07 10:54:17 +0100

    fixed issue with movement while attacking
    gave attack.gd capabilities to control movement while active withou state changing

--- objects/mechanics/finite_state_machine/states_klengan/movement/attack.gd
+++ objects/mechanics/finite_state_machine/states_klengan/movement/attack.gd
@@ -4,7 +4,10 @@ extends "res://objects/mechanics/finite_state_machine/states_klengan/movement/mo
 func enter():
 	.enter()
 	owner.set_AttackCollision_disabled(false)
-	owner.play_directional_animation("attack")
+	if velocity.x != 0:
+		owner.play_directional_animation("attack") #replace with move attack anim
+	else:
+		owner.play_directional_animation("attack") #replace with idle attack anim
 
 
 func exit():
@@ -13,4 +16,23 @@ func exit():
 
 
 func _on_animation_finished(_anim_name):
-	emit_signal("finished", "move")
+	if velocity.x != 0:
+		emit_signal("finished", "move")
+	else: 
+		emit_signal("finished", "idle")
+
+func update(delta):
+	.update(delta)
+	if velocity == Vector2() && !sneak:
+		velocity.x = 0
+	elif sneak:
+		velocity.x = MAX_SPEED*0.5
+
+
+func apply_forces():
+	.apply_forces()
+	var input_direction = get_input_direction()
+	if input_direction == Vector2():
+		velocity.x = int(lerp(velocity.x, 0, 0.4))
+	else:
+		velocity.x = clamp(velocity.x + input_direction.x * ACCELERATON, -MAX_SPEED, MAX_SPEED)
