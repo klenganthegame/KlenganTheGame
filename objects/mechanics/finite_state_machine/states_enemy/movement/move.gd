4b17e51664d1eba36d8f2527488656ba58f22385
Sven Iffland <sven@aiffland.de>
2020-02-17 21:52:13 +0100

    enemy wander state für rumlaufen wenn idle
    klappt aber noch nicht ganz

--- objects/mechanics/finite_state_machine/states_enemy/movement/move.gd
+++ objects/mechanics/finite_state_machine/states_enemy/movement/move.gd
@@ -1,13 +1,11 @@
 extends "res://objects/mechanics/finite_state_machine/states_enemy/movement/movement.gd"
 
-export(float, 0, 1) var JUMP_PCT = 0.02
-
+export(float, 0, 1) var JUMP_PCT = 0.03
+export(float, 0, 1) var IDLING_MOVE_PCT = 0.4
 
 func update(_delta):
 	.update(_delta)
 	velocity = owner.move_and_slide(velocity, Vector2(0, -1))
-	
-	try_jump()
 	
 #	#Do not use this, buggy as hell:)
 #	if Input.is_action_just_pressed("attack"):
@@ -15,23 +13,35 @@ func update(_delta):
 
 
 func try_jump():
-	if owner.is_on_floor() and randf() < JUMP_PCT:
+	if owner.is_on_floor() and velocity.x != 0 and randf() < JUMP_PCT:
 		emit_signal("finished", "jump")
 
 
 func apply_forces():
 	.apply_forces()
-	approach_player()
+	if owner.focused_body != null:
+		approach_player()
+	else:
+		 wander()
 
 
 func approach_player():
-	if owner.focused_body != null:
-		var player_right = owner.position.x - owner.focused_body.position.x < 0
-		var move_acceleration = int(pow(-1, int(!player_right)) * ACCELERATON)
-		velocity.x = clamp(velocity.x + move_acceleration, -200, 200)
+	var player_right = owner.position.x - owner.focused_body.position.x < 0
+	var move_acceleration = int(pow(-1, int(!player_right)) * ACCELERATON)
+	velocity.x = clamp(velocity.x + move_acceleration, -200, 200)
+	
+	if velocity.x != 0:
+		owner.set_looking_right(velocity.x > 0)
+		owner.play_directional_animation("move")
+	try_jump()
+
+func wander():
+	if rand_range(-1,1) < IDLING_MOVE_PCT:
+		velocity.x = clamp(velocity.x + rand_range(-0.8,0.8), -200, 200)
 		
 		if velocity.x != 0:
 			owner.set_looking_right(velocity.x > 0)
 			owner.play_directional_animation("move")
 	else:
 		velocity.x = int(lerp(velocity.x, 0, LERP_FACTOR))
+		emit_signal("finished","idle")
