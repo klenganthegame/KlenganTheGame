7195017001b32af0a4575485f1cc3bca64c6f708
Max Schmitt <max98tonton@gmail.com>
2020-02-27 16:09:36 +0100

    mobile rumble support

--- scripts/RumbleHandler.gd
+++ scripts/RumbleHandler.gd
@@ -1,10 +1,13 @@
 extends Node
 
 func rumble_normal_attack():
+	Input.vibrate_handheld(0.2)
 	Input.start_joy_vibration(0, 1, 0, 0.2)
 
 func rumble_dash():
 	Input.start_joy_vibration(0, 0.3, 1, 0.2)
+	Input.vibrate_handheld(0.4)
 
 func rumble_hit():
 	Input.start_joy_vibration(0, 1, 0, 0.2)
+	Input.vibrate_handheld(0.1)
