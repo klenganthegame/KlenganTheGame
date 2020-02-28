70421b19fc49e4423cbb1bd9bf9eed82365d001f
Max Schmitt <max98tonton@gmail.com>
2020-02-29 00:37:51 +0100

    fixed rumble with joycon

--- scripts/RumbleHandler.gd
+++ scripts/RumbleHandler.gd
@@ -3,14 +3,17 @@ extends Node
 func rumble_normal_attack():
 	if OS.get_name() == "android" or OS.get_name() == "iOS" or OS.get_name() == "Blackberry 10":
 		Input.vibrate_handheld(0.2)
+	elif Input.get_joy_name(0) != "":
 		Input.start_joy_vibration(0, 1, 0, 0.2)
 
 func rumble_dash():
 	if OS.get_name() == "android" or OS.get_name() == "iOS" or OS.get_name() == "Blackberry 10":
-		Input.start_joy_vibration(0, 0.3, 1, 0.2)
 		Input.vibrate_handheld(0.4)
+	elif Input.get_joy_name(0) != "":
+		Input.start_joy_vibration(0, 0.3, 1, 0.2)
 
 func rumble_hit():
 	if OS.get_name() == "android" or OS.get_name() == "iOS" or OS.get_name() == "Blackberry 10":
-		Input.start_joy_vibration(0, 1, 0, 0.2)
 		Input.vibrate_handheld(0.1)
+	elif Input.get_joy_name(0) != "":
+		Input.start_joy_vibration(0, 1, 0, 0.2)
