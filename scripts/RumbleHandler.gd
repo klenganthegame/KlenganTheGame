36b43562b6b0ac03bd1b1127e2d731ee34aee845
Max Schmitt <max98tonton@gmail.com>
2020-02-29 12:54:31 +0100

    SaveGame, RumbleHandler Docstrings

--- scripts/RumbleHandler.gd
+++ scripts/RumbleHandler.gd
@@ -1,12 +1,22 @@
+"""
+Handles Rumble of Phone and Mobile
+"""
+
 extends Node
 
 func rumble_normal_attack():
+	"""
+	Attack Rumble
+	"""
 	if OS.get_name() == "android" or OS.get_name() == "iOS" or OS.get_name() == "Blackberry 10":
 		Input.vibrate_handheld(0.2)
 	if Input.get_joy_name(0) != "":
 		Input.start_joy_vibration(0, 0.7, 0.1, 0.1)
 
 func rumble_dash():
+	"""
+	Dash Rumble
+	"""
 	if OS.get_name() == "android" or OS.get_name() == "iOS" or OS.get_name() == "Blackberry 10":
 		Input.vibrate_handheld(0.4)
 	if Input.get_joy_name(0) != "":
@@ -14,6 +24,9 @@ func rumble_dash():
 		
 
 func rumble_hit():
+	"""
+	Hit Rumble
+	"""
 	if OS.get_name() == "android" or OS.get_name() == "iOS" or OS.get_name() == "Blackberry 10":
 		Input.vibrate_handheld(0.1)
 	if Input.get_joy_name(0) != "":
