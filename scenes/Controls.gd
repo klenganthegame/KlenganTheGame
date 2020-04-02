e1acb53b9b74d6743521422adb4a8ace4dfb3443
Max Schmitt <max98tonton@gmail.com>
2020-04-02 23:27:20 +0200

    Better Controlls and UI Control Changes

--- scenes/Controls.gd
+++ scenes/Controls.gd
@@ -2,6 +2,15 @@ extends Control
 
 signal closed()
 
+func _ready():
+	
+	if Input.get_joy_name(0) != "":
+		_on_JoyPad_pressed()
+	else:
+		_on_Keyboard_pressed()
+		
+	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
+	
 func show_controlls():
 	$Back.grab_focus()
 	$AnimationPlayer.play("show")
@@ -10,7 +19,21 @@ func hide_controlls():
 	$AnimationPlayer.play("hide")
 	emit_signal("closed")
 
-
 func _on_Back_pressed():
 	hide_controlls()
-	pass # Replace with function body.
+
+
+func _on_JoyPad_pressed():
+	$Joypad.show()
+	$Keyboard.hide()
+
+
+func _on_Keyboard_pressed():
+	$Joypad.hide()
+	$Keyboard.show()
+
+func _on_joy_connection_changed(device_id, connected):
+	if connected:
+		_on_JoyPad_pressed()
+	else:
+		_on_Keyboard_pressed()
