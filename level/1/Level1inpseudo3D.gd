9361909098e12ff02a7874f92708a76dc9dff184
Max Schmitt <max98tonton@gmail.com>
2020-04-08 16:52:32 +0200

    music in pseudo3D

---
+++ level/1/Level1inpseudo3D.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	AudioHandler.play_music("BackstageDancer")
+	$GameLayer/Klengan.talk(["Okay... ",
+	"Das ist also dieser...",
+	"Backstage Bereich...",
+	"Wie komm ich hier wieder weg?",
+	"Ich glaube ich habe mich verlaufen..."])
