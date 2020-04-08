00efa12e826b8b4e7e8a8119943c457aa3cb9c19
Max Schmitt <max98tonton@gmail.com>
2020-04-08 16:05:13 +0200

    play music when starting
    so much BackstageDance so much wow

--- level/1/Level1.gd
+++ level/1/Level1.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
-#func _ready():
-#	$Klengan.talk(["Klengan: Okay... ",
-#	"Wo bin ich?"])
+func _ready():
+	AudioHandler.play_music("BackstageDancer")
+	$Klengan.talk(["Okay... ",
+	"Das ist also dieser...",
+	"Backstage Bereich...",
+	"Wie komm ich hier wieder weg?",
+	"Ich glaube ich habe mich verlaufen..."])
