ea50706638ece289fbe1e0207959b667a2ec5b50
Max Schmitt <max98tonton@gmail.com>
2020-09-26 18:37:24 +0200

    jump animation

--- scenes/characters/klengan/fsm/states_klengan/movement/Doublejump.gd
+++ scenes/characters/klengan/fsm/states_klengan/movement/Doublejump.gd
@@ -2,6 +2,7 @@ extends StateKlenganMovement
 class_name StateKlenganDoublejump
 
 func enter():
+	owner.get_node("AnimatedSprite").frame = 0
 	.enter()
 	velocity.y = -500
 
@@ -24,4 +25,4 @@ func apply_forces():
 			velocity.x = clamp(velocity.x + input_direction.x * ACCELERATON, -MAX_SPEED/4*1.5, MAX_SPEED/4*1.5)
 		else:
 			velocity.x = clamp(velocity.x + input_direction.x * ACCELERATON, -MAX_SPEED, MAX_SPEED)
-		owner.play_directional_animation("walk", (input_direction.x > 0))
+		owner.play_directional_animation("jump", (input_direction.x > 0))
--- scenes/characters/klengan/fsm/states_klengan/movement/StateKlenganJump.gd
+++ scenes/characters/klengan/fsm/states_klengan/movement/StateKlenganJump.gd
@@ -29,4 +29,4 @@ func apply_forces():
 			velocity.x = clamp(velocity.x + input_direction.x * ACCELERATON, -MAX_SPEED/4*1.5, MAX_SPEED/4*1.5)
 		else:
 			velocity.x = clamp(velocity.x + input_direction.x * ACCELERATON, -MAX_SPEED, MAX_SPEED)
-		owner.play_directional_animation("walk", (input_direction.x > 0))
+		owner.play_directional_animation("jump", (input_direction.x > 0))
